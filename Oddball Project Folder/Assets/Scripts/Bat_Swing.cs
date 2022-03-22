252606b5d4e713189a80fbe3b3b781bb853ac305
Noah <noahmtgamedesign@gmail.com>
2022-03-22 15:54:59 +1100

    Project Folder Upload
    Uploading the project folder to the main

---
+++ Oddball Project Folder/Assets/Scripts/Bat_Swing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat_Swing : MonoBehaviour
+{
+
+    public Animator animBat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animBat = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            animBat.Play("Bat_Swing");
+        }
+    }
+}
